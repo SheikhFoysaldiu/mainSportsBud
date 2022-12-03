9f614898f50e1309a57f9f2f898673aba84f9b56
sakir101 <sakirhossainf99@gmail.com>
2022-12-03 23:04:18 +0600

    welcome page

--- src/Pages/Welcome/Welcome.js
+++ src/Pages/Welcome/Welcome.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import backgroundImage from '../../Asset/welcome/welcome.jpg'
+import './Welcome.css'
+import person from '../../Asset/person/person.png'
 
 const Welcome = () => {
+
     return (
-        <div>
-            
-        </div>
+
+        <section className='main-container w-full h-screen '>
+            <div className='image-overlay flex flex-col justify-center items-center'>
+                <div className="avatar">
+                    <div className="w-24 rounded-full">
+                        <img src={person} alt='' />
+                    </div>
+                </div>
+                <h2 className='text-white text-4xl font-bold my-3'>Welcome to Sportzbud</h2>
+                <button className="btn btn-outline btn-success my-3 w-1/2 text-xl rounded-full">Login</button>
+                <button className="btn btn-outline btn-warning my-3 w-1/2 text-xl rounded-full">Signup</button>
+
+
+            </div>
+        </section>
+
+
+
     );
 };
 
