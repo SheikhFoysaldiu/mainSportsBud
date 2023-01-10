20bed7e2f6265f85822d57f3c8cbee20d7739076
unknown <foysal15-3442@diu.edu.bd>
2023-01-11 01:50:37 +0600

    Infinity Scroll React Query

--- src/Pages/Home/SportsInteresets/SportInterests.js
+++ src/Pages/Home/SportsInteresets/SportInterests.js
@@ -4,16 +4,15 @@ import { useParams } from "react-router-dom";
 import Loading from "../../../Shared/Loading/Loading";
 import InfiniteScroll from 'react-infinite-scroll-component';
 import SingleSport from "./SingleSport";
-
 import { API_URL } from "../../../API/config";
 import { useInfiniteQuery } from '@tanstack/react-query'
 const SportInterests = () => {
   const [sports, setSports] = useState([]);
-  // const [skip, setSkip] = useState(0);
-  const take = 1;
+  const [skip, setSkip] = useState(0);
+  const take = 6;
   const apiPath = `${API_URL}/api/v1/sport/sports`
 
-  const fetchSports = async ({ skip = 0 }) => {
+  const fetchSports = async () => {
     const queryParam = "?skip=" + skip + "&take=" + take;
     const url = apiPath + queryParam;
     console.log(url)
@@ -27,6 +26,7 @@ const SportInterests = () => {
 
     setSports(sports => [...sports, data.sports]);
     // setSkip(sports.length)
+    setSkip(skip + take);
     return data.sports;
   }
 
@@ -42,7 +42,14 @@ const SportInterests = () => {
   } = useInfiniteQuery({
     queryKey: ['sports'],
     queryFn: fetchSports,
-    getNextPageParam: (params) => console.log(params)
+    getNextPageParam: (lastPage, pages) => {
+      console.log("lastPage:", lastPage)
+      console.log("pages:", pages)
+      if (lastPage.length < take) {
+        return undefined;
+      }
+      return skip;
+    }
   })
 
 
@@ -53,47 +60,71 @@ const SportInterests = () => {
   console.log("sports:", sports)
 
   return (
-    // <div className="h-screen w-full overflow-auto lg:overflow-hidden lg:hover:overflow-auto" id="scrollableDiv">
-    //   <InfiniteScroll dataLength={sports.length}
-    //     scrollableTarget="scrollableDiv"
-    //     next={fetchSports}
-    //     loader={<Loading></Loading>}>
-    //     {/* className="grid grid-cols-1 lg:grid-cols-3 gap-4 lg:gap-8 mb-48 lg:mb-56 mt-16 mx-0 lg:mx-28" */}
-    //     <div >
-    //       {
-    //         sports.map(sport => <SingleSport key={sport.id} sport={sport}></SingleSport>)
-    //       }
-    //     </div>
-    //   </InfiniteScroll>
-    // </div>
-    <>
+    <div className="h-screen w-full overflow-auto lg:overflow-hidden lg:hover:overflow-auto" id="scrollableDiv">
+      <InfiniteScroll
+        dataLength={sports.length}
+        next={fetchNextPage}
+        scrollableTarget="scrollableDiv"
+        hasMore={hasNextPage}
+        loader={<h4>Loading...</h4>}
 
-      <div>
-        <button
-          onClick={() => fetchNextPage({ skip: 1 })}
-          id="scrollableDiv"
-          disabled={!hasNextPage || isFetchingNextPage}
-        >
+      >
+
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 lg:gap-8 mb-48 lg:mb-56 mt-16 mx-0 lg:mx-28">
+          {
+            data &&
+            data.pages.map((page, i) => (
+              <React.Fragment key={i}>
+                {page.map((sport) => (
+                  <SingleSport key={sport.id} sport={sport}></SingleSport>
+                ))}
+              </React.Fragment>
+            ))}
+
+          {error && <div>Something went wrong ...</div>}
           {isFetchingNextPage
-            ? 'Loading more...'
-            : hasNextPage
-              ? 'Load More'
-              : 'Nothing more to load'}
-        </button>
-        <div className="h-screen w-full overflow-auto lg:overflow-hidden lg:hover:overflow-auto" id="scrollableDiv">
-
-          {/* className="grid grid-cols-1 lg:grid-cols-3 gap-4 lg:gap-8 mb-48 lg:mb-56 mt-16 mx-0 lg:mx-28" */}
-          <div>
-            {
-              // sports.map(sport => <SingleSport key={sport.id} sport={sport}></SingleSport>)
-            }
-          </div>
-
+            ? 'Loading more...' : <p style={{ textAlign: "center" }}>
+              <b>No more data left!</b>
+            </p>}
         </div>
 
-      </div>
-      <div>{isFetching && !isFetchingNextPage ? 'Fetching...' : null}</div>
-    </>
+      </InfiniteScroll>
+
+    </div >
+    // <>
+    //   <button
+    //     onClick={() => fetchNextPage()}
+    //     id="scrollableDiv"
+    //     disabled={!hasNextPage || isFetchingNextPage}
+    //   >
+    //     {isFetchingNextPage
+    //       ? 'Loading more...'
+    //       : hasNextPage
+    //         ? 'Load More'
+    //         : 'Nothing more to load'}
+    //   </button>
+    //   <div>{isFetching && !isFetchingNextPage ? 'Fetching...' : null}</div>
+    //   <div className="h-screen w-full overflow-auto lg:overflow-hidden lg:hover:overflow-auto" id="scrollableDiv">
+
+    //     <div className="h-screen w-full overflow-auto lg:overflow-hidden lg:hover:overflow-auto" id="scrollableDiv">
+
+    //       {/* className="grid grid-cols-1 lg:grid-cols-3 gap-4 lg:gap-8 mb-48 lg:mb-56 mt-16 mx-0 lg:mx-28" */}
+    //       <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 lg:gap-8 mb-48 lg:mb-56 mt-16 mx-0 lg:mx-28">
+    //         {
+    //           data &&
+    //           data.pages.map((page, i) => (
+    //             <React.Fragment key={i}>
+    //               {page.map((sport) => (
+    //                 <SingleSport key={sport.id} sport={sport}></SingleSport>
+    //               ))}
+    //             </React.Fragment>
+    //           ))}
+    //       </div>
+    //     </div>
+
+    //   </div>
+
+    // </>
 
 
   )
